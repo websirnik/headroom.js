690a723de299ae8682b624c2462d6340fc5713e4
Nikita Korotaev <websirnik@gmail.com>
2016-04-15 16:06:35 +0100

    Fixes for AngularJS directive refactor
    https://github.com/WickyNilliams/headroom.js/pull/202 introduced a few issues:
    - `options.scroller` value got overridden due to the wrong order;
    - `$document` has no method querySelector;

--- src/angular.headroom.js
+++ src/angular.headroom.js
@@ -1,10 +1,16 @@
+/*!
+ * headroom.js v0.9.1 - Give your page some headroom. Hide your header until you need it
+ * Copyright (c) 2016 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * License: MIT
+ */
+
 (function (angular, Headroom) {
 
   if(!angular) {
     return;
   }
   
-  function headroom($document, HeadroomService) {
+  function headroom(HeadroomService) {
     return {
       scope: {
         tolerance: '=',
@@ -12,14 +18,14 @@
         classes: '=',
         scroller: '@'
       },
-      link: function link($scope, $element) {
+      link: function ($scope, $element) {
         var options = {};
         var opts = HeadroomService.options;
-        if (options.scroller) {
-          options.scroller = $document.querySelector(options.scroller);
-        }
         for (var prop in opts) {
           options[prop] = $scope[prop] || opts[prop];
+        }
+        if ($scope.scroller) {
+          options.scroller = document.querySelector($scope.scroller);
         }
         var headroom = new HeadroomService($element[0], options).init();
         $scope.$on('$destroy', headroom.destroy);
@@ -27,7 +33,7 @@
     };
   }
   
-  headroom.$inject = ['$document', 'HeadroomService'];
+  headroom.$inject = ['HeadroomService'];
 
   function HeadroomService() {
     return Headroom;
